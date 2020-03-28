dcc5012f54dea55de501d2ed7b0c2676eb466bd7
Mandloi7 <ritik.mandloi.2000@gmail.com>
2020-03-28 22:47:00 +0530

    Updated the Student's Page

--- app/src/main/java/com/example/squidwork/StudentPage1.java
+++ app/src/main/java/com/example/squidwork/StudentPage1.java
@@ -1,13 +1,21 @@
 package com.example.squidwork;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,41 +27,126 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/*class JobPosting {
+
+    String companyName;
+    String jobTitle;
+    String jobDescripion;
+    Long timestamp;
+
+    public JobPosting(String a, String b, String c, Long d){
+
+        this.companyName = a;
+        this.jobTitle = b;
+        this.jobDescripion = c;
+        this.timestamp = d;
+    }
+
+
+}*/
 
 public class StudentPage1 extends Fragment{
+
+    private RecyclerView applicationsRecyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<JobPosting> jobs = new ArrayList<JobPosting>();
+
+    private String TAG = "CompanyPage1";
 
     public StudentPage1() {
         // Required empty public constructor
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             final Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v=inflater.inflate(R.layout.fragment_studentpage1, container, false);
 
-        Button signOutButton =  (Button) v.findViewById(R.id.sign_out_button1);
 
-        signOutButton.setOnClickListener(new View.OnClickListener() {
+        applicationsRecyclerView = (RecyclerView) v.findViewById(R.id.applications_recycler_view);
+        applicationsRecyclerView.setHasFixedSize(true);
+        layoutManager = new GridLayoutManager(getActivity(), 1);
+
+        applicationsRecyclerView.setLayoutManager(layoutManager);
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        mAdapter = new MyAdapter(jobs);
+        applicationsRecyclerView.setAdapter(mAdapter);
+
+        db.collection("posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
-            public void onClick(View v) {
-                //make your toast here
-                mAuth.signOut();
-                mGoogleSignInClient.signOut().addOnCompleteListener(getActivity(),
-                        new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Toast.makeText(getContext(), "Signed out successfully", Toast.LENGTH_SHORT).show();
-                                getActivity().finish();
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if(e!=null){
 
-                            }
+                    Log.d(TAG, "listen:error", e);
+                    return;
 
-                        }
+                }
+                System.out.println("TADADAAA");
 
-                );
+
+                for (DocumentChange documentChange : queryDocumentSnapshots.getDocumentChanges()) {
+                    switch (documentChange.getType()) {
+                        case ADDED:
+
+                            Map docData = new HashMap();
+                            docData = documentChange.getDocument().getData();
+                            System.out.println("ADDDD "+docData);
+                            JobPosting job = new JobPosting(docData.get("companyName").toString(), docData.get("jobTitle").toString(), docData.get("jobDescription").toString(), (Long) docData.get("timeStamp"));
+
+                            jobs.add(job);
+                            jobs.sort(new Comparator<JobPosting>() {
+                                @Override
+                                public int compare(JobPosting o1, JobPosting o2) {
+                                    return o2.timestamp.compareTo(o1.timestamp);
+                                }
+                            });
+
+                            mAdapter.notifyDataSetChanged();
+
+                            break;
+                        case MODIFIED:
+
+                            break;
+                        case REMOVED:
+
+                            break;
+                    }
+                }
+
             }
         });
+
+       /* Button addPostingButton = (Button) v.findViewById(R.id.add_posting_button);
+
+        addPostingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                System.out.println("NENENE");
+                startActivity(new Intent(CompanyPage1.this.getActivity(), AddPostingFormPage.class));
+
+            }
+        });
+*/
+
 
         return v;
     }
